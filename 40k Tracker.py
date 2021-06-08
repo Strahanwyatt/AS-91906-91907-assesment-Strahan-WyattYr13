ab019b7d452ac8052bba8575102a43fc3f8ad41a
Strahanwyatt <wyatts@stu.otc.school.nz>
2021-06-09 08:59:48 +1200

    gui with a label and button using kv

--- 40k Tracker.py
+++ 40k Tracker.py
@@ -1,5 +1,16 @@
+
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+
+class BoxlayoutExample(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        b1 = Button(text="A")
+        b2 = Button(text="b")
+        self.add_widget(b1)
+        self.add_widget(b2)
 
 
 class MainWidget(Widget):
