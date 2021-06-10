b8606a45f54b2af40610eaecb200f77ac7420077
Strahanwyatt <wyatts@stu.otc.school.nz>
2021-06-11 11:00:00 +1200

    orientation and size of buttons in kivy

--- 40k Tracker.py
+++ 40k Tracker.py
@@ -4,13 +4,16 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 
-class BoxlayoutExample(BoxLayout):
-    def __init__(self, **kwargs):
+class BoxLayoutExample(BoxLayout):
+   pass
+   """ def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.orientation = "horizontal"
         b1 = Button(text="A")
         b2 = Button(text="b")
+        
         self.add_widget(b1)
-        self.add_widget(b2)
+        self.add_widget(b2)"""
 
 
 class MainWidget(Widget):
