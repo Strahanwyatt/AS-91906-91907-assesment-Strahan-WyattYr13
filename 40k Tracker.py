37f472a30ac7ff894042113598e7499092d39afd
Strahanwyatt <wyatts@stu.otc.school.nz>
2021-06-16 09:39:21 +1200

    button changing a text

--- 40k Tracker.py
+++ 40k Tracker.py
@@ -1,8 +1,17 @@
 
 from kivy.app import App
+from kivy.properties import StringProperty
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
+
+class WidgetsExample(GridLayout):
+    my_text = StringProperty("poop")
+    def on_button_click(self):
+        print("Button clicked")
+        self.my_text = "you shat yourself"
+
 
 class BoxLayoutExample(BoxLayout):
    pass
@@ -15,6 +24,8 @@ class BoxLayoutExample(BoxLayout):
         self.add_widget(b1)
         self.add_widget(b2)"""
 
+class GridLayoutExample(GridLayout):
+    pass
 
 class MainWidget(Widget):
     pass
