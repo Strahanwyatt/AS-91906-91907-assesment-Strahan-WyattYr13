c4428bd5004027101e8a70a708455dd3a571345a
Strahanwyatt <wyatts@stu.otc.school.nz>
2021-06-17 13:53:57 +1200

    class bluepint for a counter

--- 40k Tracker.py
+++ 40k Tracker.py
@@ -11,7 +11,7 @@ class WidgetsExample(GridLayout): #blueprint
         self.count = count
         self.my_text = my_text
 
-        StringProperty("VICTORY POINTS")
+        StringProperty("VICTORY POINTS"  )
 
 
 
