15e4221a93826790129dd19185df149ebccdf846
Strahanwyatt <wyatts@stu.otc.school.nz>
2021-06-21 15:19:27 +1200

    rewriting code

--- 40k Tracker.py
+++ 40k Tracker.py
@@ -1,52 +1,24 @@
-
 from kivy.app import App
-from kivy.properties import StringProperty
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.widget import Widget
 from kivy.uix.button import Button
+from kivy.uix.widget import Widget
 
-class WidgetsExample(GridLayout): #blueprint
+class warhammerhelper(App):
+    def build(self):
+        return Button(text='hello world')
+
+if __name__ =='__main__':
+    warhammerhelper().run
+
+
+"""class WidgetsExample(GridLayout): #blueprint
     def __init__(self, count, my_text):
         self.count = count
         self.my_text = my_text
 
-        StringProperty("VICTORY POINTS"  )
-
-
 
     def on_button_click(self): # button
         print("button clicked")
-        self.count += 1
-        self.my_text = str(self.count )
+        StringProperty("VICTORY POINTS" )
+        self.countup += 1"""
 
 
-
-
-
-    "button.config"
-    "self.my_text = str(self.count VP."
-    "self.count += 1"
-
-
-class BoxLayoutExample(BoxLayout):
-   pass
-   """ def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.orientation = "horizontal"
-        b1 = Button(text="A")
-        b2 = Button(text="b")
-        
-        self.add_widget(b1)
-        self.add_widget(b2)"""
-
-class GridLayoutExample(GridLayout):
-    pass
-
-class MainWidget(Widget):
-    pass
-
-class WarhammerHelperApp(App):
-    pass
-
-WarhammerHelperApp().run()
