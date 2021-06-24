b1353c25cf3574411b7637a193564882412e07ac
Strahanwyatt <wyatts@stu.otc.school.nz>
2021-06-25 10:59:49 +1200

    buttons work with gui just need to make the gui change

--- 40k Tracker.py
+++ 40k Tracker.py
@@ -1,17 +1,20 @@
 from operator import pos
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
+from kivy.core.audio import SoundLoader
+
+class GridLayoutExample(GridLayout):
+    pass
 
 
+class VPcounter(GridLayout):
+    def on_Button_click1(self):
+        print("blood for the blood god ")
 
-
-
-class VPcounter(Widget):
-
-
-
-
+    def on_Button_click2(self):
+        print("skulls for the skull throne")
 
 class Mainwidget(Widget):
     pass
