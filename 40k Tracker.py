fce40a1be09430371fca60d74981b5d0f662ef6d
Strahanwyatt <wyatts@stu.otc.school.nz>
2021-06-24 13:26:35 +1200

    gui is working as the .kv file hadels the gui i need to make a class that changes the label values with +1 and -1 buttons

--- 40k Tracker.py
+++ 40k Tracker.py
@@ -1,24 +1,22 @@
+from operator import pos
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 
-class warhammerhelper(App):
-    def build(self):
-        return Button(text='hello world')
-
-if __name__ =='__main__':
-    warhammerhelper().run
 
 
-"""class WidgetsExample(GridLayout): #blueprint
-    def __init__(self, count, my_text):
-        self.count = count
-        self.my_text = my_text
 
 
-    def on_button_click(self): # button
-        print("button clicked")
-        StringProperty("VICTORY POINTS" )
-        self.countup += 1"""
+class VPcounter(Widget):
 
 
+
+
+
+class Mainwidget(Widget):
+    pass
+class warhammerhelperApp(App):
+    pass
+
+
+warhammerhelperApp().run()
