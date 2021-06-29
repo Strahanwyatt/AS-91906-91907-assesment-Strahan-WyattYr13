d346b14bde5ad3f39ece461296b86fa25bf24b23
Strahanwyatt <wyatts@stu.otc.school.nz>
2021-06-30 09:17:00 +1200

    working victory point counter

--- 40k Tracker.py
+++ 40k Tracker.py
@@ -1,6 +1,8 @@
 from operator import pos
 from kivy.app import App
+from kivy.properties import StringProperty
 from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
 from kivy.core.audio import SoundLoader
@@ -9,12 +11,27 @@ class GridLayoutExample(GridLayout):
     pass
 
 
+
 class VPcounter(GridLayout):
+    labelnumber = 1
+    vp_text = StringProperty(" Victory points" "1")
+
+    self.plusone_button = Button(text = +1)
+
+    self
+
     def on_Button_click1(self):
-        print("blood for the blood god ")
+        print("blood for the blood god")
+        self.labelnumber += 1
+        self.vp_text = str(self.labelnumber)
+
 
     def on_Button_click2(self):
         print("skulls for the skull throne")
+        self.labelnumber -= 1
+        self.vp_text = str(self.labelnumber)
+
+
 
 class Mainwidget(Widget):
     pass
